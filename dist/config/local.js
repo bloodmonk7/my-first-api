b3eb9a0573cd0d11b8ac87f3b89d4417f17ef896
Ritik Singh <48784848+bloodmonk7@users.noreply.github.com>
2023-01-01 21:25:15 +0530

    Delete dist directory

--- dist/config/local.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports["default"] = {
-    port: 3001
-};
-//# sourceMappingURL=local.js.map
--- dist/config/prod.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports["default"] = {
-    port: process.env.PORT
-};
-//# sourceMappingURL=prod.js.map
